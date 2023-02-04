d7bd06219d6e156dda07f335009c05a6a25aeef2
joaquinxtomas <jtppalacios@gmail.com>
2023-02-04 18:23:29 -0300

    second deploy

--- app.js
+++ app.js
@@ -44,7 +44,9 @@ app.use('/user', rutaUser);
 
 app.use('/product', rutaProduct);
 
-app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
+const port = process.env.PORT || 3022
+
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 //error 404
 app.use((req,res,next)=>{
     res.status(404).render("not-found")
